69264a5c5526160f9823fa356e8016fef1d0d1aa
Artyom Khokhlov <artyom.khokhlov@capital.com>
2024-04-12 23:54:45 +0200

    added sum of three integers task

---
+++ pygen-beginner-course/module_2_Input-output/sum-of-three-integers.py
@@ -0,0 +1,15 @@
+"""
+Sum of Three Numbers
+Write a program that reads three integers and displays their sum on the screen. Each number is written on a separate line.
+
+Input Format
+The program receives three integers, each on a separate line.
+
+Output Format
+The program should output the sum of the entered numbers.
+"""
+
+a = int(input())
+b = int(input())
+c = int(input())
+print(a + b + c)
