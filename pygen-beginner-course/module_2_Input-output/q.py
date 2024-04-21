db17b62ae3d71176961263841df5664dac640421
Artyom Khokhlov <artyom.khokhlov@capital.com>
2024-04-21 15:10:50 +0200

    added arithmetic-operations task

--- pygen-beginner-course/module_2_Input-output/q.py
+++ pygen-beginner-course/module_2_Input-output/q.py
@@ -1,3 +1,14 @@
+"""
+Arithmetic Operations
+Write a program that calculates the sum, difference, and product of two integers entered from the keyboard.
+
+Input format
+The program receives two integers as input, each on a separate line.
+
+Output format
+The program should output the sum, difference, and product of the entered numbers, each on a separate line.
+"""
+
 a = int(input())
 b = int(input())
 print(a, "+", b, "=", a + b)
