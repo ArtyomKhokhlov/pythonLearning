a6c62593c42fd8730aee271575fcca48f4488553
Artyom Khokhlov <artyom.khokhlov@capital.com>
2024-05-18 10:03:39 +0200

    added distance-in-meters task

---
+++ pygen-beginner-course/module_2_Input-output/distance-in-meters.py
@@ -0,0 +1,14 @@
+"""
+Distance in Meters
+Write a program that finds the total number of meters for a given number of centimeters.
+
+Input format:
+The program receives a natural number - the number of centimeters.
+
+Output format:
+The program should output a single number - the total number of meters.
+"""
+
+a = int(input())
+b = a // 100
+print(b)
